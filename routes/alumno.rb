40cc9a0a969d1c8060e6d6f493eed69d4fd0a139
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-16 20:21:22 -0500

    Obtener alumno y emplado por id

--- routes/alumno.rb
+++ routes/alumno.rb
@@ -2,4 +2,8 @@ class MyApp < Sinatra::Base
   get '/alumno/listar' do
     Alumno.to_a.to_json
   end
+
+  get '/alumno/obtener/:id' do
+    Alumno.select(:id, :codigo, :dni, :nombres, :paterno, :materno, :correo_personal, :correo_alumno, :celular).where(:id => params[:id]).to_a[0].to_json
+  end
 end
--- routes/empleado.rb
+++ routes/empleado.rb
@@ -2,4 +2,8 @@ class MyApp < Sinatra::Base
   get '/empleado/listar' do
     Empleado.to_a.to_json
   end
+
+  get '/empleado/obtener/:id' do
+    Empleado.select(:id, :codigo, :dni, :nombres, :paterno, :materno, :correo_personal, :correo_laboral, :celular).where(:id => params[:id]).to_a[0].to_json
+  end
 end
